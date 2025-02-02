0acf9cb99d708989b321bbb26befa12114976d7f
DESKTOP-VK39TTE\s <guichanist@gmail.com>
2025-02-03 04:56:41 +0900

    add webm

--- chatbotapi/chatbotapi/settings.py
+++ chatbotapi/chatbotapi/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-(3h)w9n2f*vtv@e0lb=g^pfnmq5-ba%ovfvf!!kkv6+*zt(x0o
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
